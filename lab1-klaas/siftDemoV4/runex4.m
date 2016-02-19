940afb962418fd4ae7c79298ec3ecb34eb6ee5c3
Klaas Kliffen <klaasyk@users.noreply.github.com>
2016-02-19 15:46:38 +0100

    lab 1 excs 2,4 code done

---
+++ lab1-klaas/siftDemoV4/runex4.m
@@ -0,0 +1,10 @@
+matches0 = match("scene.pgm","book.pgm");
+matches1 = match("scene.pgm","books25.pgm");
+matches2 = match("scene.pgm","books50.pgm");
+matches3 = match("scene.pgm","books100.pgm");
+matches4 = match("scene.pgm","books200.pgm");
+matches5 = match("scene.pgm","books300.pgm");
+matches6 = match("scene.pgm","books400.pgm");
+
+matches = [matches0 matches1 matches2 matches3 matches4 matches5 matches6]
+% 98   100    91    42     3     0     0
